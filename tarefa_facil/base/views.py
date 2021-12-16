88bd3ea927197d8371faf632d7093070ed8ba6a9
GetulioCastroJr <augustoge71@gmail.com>
2021-12-15 23:45:07 -0300

    melhorado o layout da página home.html

--- tarefa_facil/base/views.py
+++ tarefa_facil/base/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'base/home.html')
+
+def tarefa_listar(request):
+    return render(request, 'base/tarefa_listar.html')
+
